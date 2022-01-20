46853b92fcbe978aff719e9d2dd3401db54c4b72
leshista <a.leshista@gmail.com>
2022-01-20 19:42:08 +0300

    Animation for burger

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const burger = document.querySelector('.burger');
-const burgerStripes = document.querySelectorAll('.burger__stripes');
+const burgerStripes = document.querySelectorAll('.burger__stripe');
 const burgerMenu = document.querySelector('.burger-menu');
 
 burger.addEventListener('click', () => {
-    // burgerStripes.forEach((e, i, a) => {});
+    burgerStripes.forEach((e, i, a) => {
+        e.classList.toggle('stripe-active');
+    });
     burgerMenu.classList.toggle('hidden');
 });
