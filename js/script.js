c148b154e540c1c7c7836f4b8feda98bc66b1495
leshista <a.leshista@gmail.com>
2022-01-23 19:19:31 +0300

    Click on burger-menu__item now hiddens the burger menu

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 const burger = document.querySelector('.burger');
 const burgerStripes = document.querySelectorAll('.burger__stripe');
 const burgerMenu = document.querySelector('.burger-menu');
+const burgerItems = document.querySelectorAll('.nav__item_burger');
 
 burger.addEventListener('click', () => {
-    burgerStripes.forEach((e, i, a) => {
+    burgerStripes.forEach((e) => {
         e.classList.toggle('stripe-active');
     });
     burgerMenu.classList.toggle('hidden');
+});
+
+burgerItems.forEach((e) => {
+    e.addEventListener('click', () => {
+        burgerMenu.classList.toggle('hidden');
+    });
 });
